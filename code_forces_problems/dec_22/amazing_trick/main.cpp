c7fc3ec3d4c907126b9e071feaad388fb26cd569
mhosnyessa <mhosny67710@gmail.com>
2022-12-07 16:32:46 +0200

    problem a gave up lol

---
+++ code_forces_problems/dec_22/amazing_trick/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+ 
+int main()
+{
+	// rule a[p[q[i]]] = i
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i = 0; i < n; i++){
+		cin>>a[i];
+	}
+	int p[n];
+	int q[n];
+	//for everyvalue of p
+	for(int ip=0;ip<n;ip++){}
+
+
+	return 0;
+}
+// gave up for now lol
+/* link for the problem
+ *https://codeforces.com/contest/1773/problem/A
+ */
